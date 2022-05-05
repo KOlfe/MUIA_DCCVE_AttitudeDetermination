ea3791358dd59fba63a8c5d02347210ba04a5cb5
Karl Olfe <kolfe@eusoc.upm.es>
2022-05-05 11:47:35 +0200

    Initial status After 2022 DCCVE lessons

---
+++ desktop/Q_function.m
@@ -0,0 +1,32 @@
+function [R,Q] = Q_function(vectors_body, vectors_inertial)
+% Finds the rotation matrix and the quaternion given 2 vectors readings 
+%   in body coordinates using the Q method.
+
+%
+% Inputs            Description                                         Units
+%   vectors_body    Array[3x2] : 2 vectors in Body coordinates.         [N/A]
+%       vectors_body(:,1) : sun vector
+%       vectors_body(:,2) : magnetometer vector                         Units
+%   vectors_inertial    Array[3x2] : 2 vectors in inertial coordinates. [N/A]
+%       vectors_inertial(:,1) : sun vector
+%       vectors_inertial(:,2) : magnetometer vector
+% Outputs
+%   R               Array[3x3] : attitude rotation matrix               [N/A]
+%   Q               Array[4x1] : attitude quaternion                    [N/A]
+
+%% To add noise uncomment the following lines:
+%     % sun vector with gaussian noise:
+%     vectors_body(:,1) = vectors_body(:,1) + randn(3,1)/50;
+%     % sun vector with bias noise:
+%     vectors_body(:,1) = vectors_body(:,1) + 0.1*ones(3,1);
+
+%     % magnetic vector with gaussian noise:
+%     vectors_body(:,2) = vectors_body(:,2) + randn(3,1)/50;
+%     % magnetic vector with bias noise:
+%     vectors_body(:,2) = vectors_body(:,2) + 0.1*ones(3,1);
+
+%% Q algorithm
+R = [1 0 0; 0 1 0; 0 0 1];
+Q = [1; 0; 0; 0];
+end
+
---
+++ desktop/startQ.sh
@@ -0,0 +1,1 @@
+matlab -nodesktop -r "addpath '/opt/42/muia_dccve/desktop'; Q_program"
